ac5372447af42329f4e57fd021773dae3fd88347
mauroluciano <mauroluciano911@gmail.com>
2023-08-24 17:22:19 +0200

    add const team

---
+++ js/functions.js
@@ -0,0 +1,35 @@
+// creazione array
+const team = [
+  {
+    name: "Wayne Barnett",
+    position: "Founder & CEO",
+    pic: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    name: "Angela Caroll",
+    position: "Chief Editor",
+    pic: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter Gordon",
+    position: "Office Manager",
+    pic: "walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela Lopez",
+    position: "Social Media Manager",
+    pic: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott Estrada",
+    position: "Developer",
+    pic: "scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara Ramos",
+    position: "Graphic Designer",
+    pic: "barbara-ramos-graphic-designer.jpg",
+  },
+];
+
+console.log(team);
